d5ce42135f3bfb476a12d071a2e5490b18840040
ostolopov <egor21231bar@gmail.com>
2024-12-11 11:25:20 +0300

    update

--- laba2 pleshk/main.c
+++ laba2 pleshk/main.c
@@ -39,7 +39,7 @@ void arr_first_inicialization (int **arr, int *arr_size)
     int arr_size_local = 0;
     int arr_element;
     printf("Введите размер массива: \n");
-    get_int(&arr_size_local, 0, INT_MAX);
+    get_int(&arr_size_local, 1, INT_MAX);
     *arr = realloc(*arr, arr_size_local * sizeof(int));
     for (int i = 0; i < arr_size_local; i++)
     {
@@ -56,15 +56,32 @@ void arr_second_inicialization (int ***arr, int *n, int *m)
     int m_local = 0;
     int arr_element;
     printf("Введите количество строк: \n");
-    get_int(&n_local, 0, INT_MAX);
+    get_int(&n_local, 1, INT_MAX);
     printf("Введите количество столбцов: \n");
-    get_int(&m_local, 0, INT_MAX);
-    **arr = malloc(n_local * sizeof(int));
-    for (int i = 0; i < m_local; )
-    
-    
+    get_int(&m_local, 1, INT_MAX);
+    *arr = malloc(n_local * sizeof(int*));
+    for (int i = 0; i < n_local; i++)
+    {
+        (*arr)[i] = malloc(m_local * sizeof(int));
+    }
+    for (int i = 0; i < n_local; i++)
+    {
+        for (int j = 0; j < m_local; j++)
+        {
+            printf("arr[%d][%d] = ", i, j);
+            get_int(&arr_element, INT_MIN, INT_MAX);
+            (*arr)[i][j] = arr_element;
+        }
+    }
     *n = n_local;
     *m = m_local;
+}
+
+void arr_second_free(int **arr, int n) {
+    for (int i = 0; i < n; i++) {
+        free(arr[i]);
+    }
+    free(arr);
 }
 
 void arr_first_print (int *arr, int arr_size)
@@ -84,7 +101,19 @@ void arr_first_print (int *arr, int arr_size)
 
 void arr_second_print (int **arr, int n, int m)
 {
-    
+    if (n == 0 || m == 0)
+    {
+        printf("Массив не инициализирован\n");
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%3.d ", arr[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main(void) {
@@ -95,7 +124,7 @@ int main(void) {
     int arr_size_y = 0;
     int case_number = 0;
     printf("(1) Инициализация одномерного массива.\n(2) Инициализация двумерного массива.\n(3) Вывод одномерного массива.\n(4) Вывод двумерного массива.\n\n");
-    while (get_int(&case_number, 0 ,7) != -1 && case_number != 0)
+    while (get_int(&case_number, 0 ,7) != -1  && case_number != 0)
     {
         switch (case_number)
         {
@@ -107,6 +136,8 @@ int main(void) {
             case 3:
                 arr_first_print(arr_first, arr_size);
                 break;
+            case 4:
+                arr_second_print(arr_second, arr_size_x, arr_size_y);
             default:
                 printf("Жмакнули не ту кнопку\n");
                 break;
@@ -114,6 +145,6 @@ int main(void) {
         printf("(1) Инициализация одномерного массива.\n(2) Инициализация двумерного массива.\n(3) Вывод одномерного массива.\n(4) Вывод двумерного массива.\n\n");
     }
     free(arr_first);
-    free(arr_second);
+    arr_second_free(arr_second, arr_size_x);
     return 0;
 }
